7c8ed72b4370faf41e1305b7010b744f37d77165
rbarbazz <raphael.barbazza@gmail.com>
2020-01-30 18:07:35 -0800

    added production config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import path from 'path';
 
 import { notesRouter } from './routes/notes';
 
@@ -9,7 +10,9 @@ import { notesRouter } from './routes/notes';
 const app = express();
 export const port = process.env.port || 8000;
 export const appUrl = `http://127.0.0.1:${port}`;
+const staticFolder = [__dirname, '../../client/build'];
 
+app.use(express.static(path.join(...staticFolder)));
 app.use(bodyParser.json());
 
 app.use('/api/notes', notesRouter);
