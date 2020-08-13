6641ff24a382366f2fa5e4f056886f1bfcf7aa5c
rbarbazz <raphael.barbazza@gmail.com>
2020-08-12 23:04:53 -0700

    test: fixed tests

--- client/src/features/Notes/Notepad.test.tsx
+++ client/src/features/Notes/Notepad.test.tsx
@@ -1,10 +1,25 @@
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import React from 'react';
 
 import { Notepad } from './Notepad';
+import reducers from '../../app/rootReducer';
+
+const Store: React.FC<{ children: JSX.Element[] | JSX.Element }> = ({
+  children,
+}) => {
+  const store = createStore(reducers);
+
+  return <Provider store={store}>{children}</Provider>;
+};
 
 test('renders learn react link', () => {
-  const { getByText } = render(<Notepad />);
+  const { getByText } = render(
+    <Store>
+      <Notepad />
+    </Store>,
+  );
   const addBtn = getByText(/add/i);
 
   expect(addBtn).toBeInTheDocument();
--- server/src/routes/notes.test.ts
+++ server/src/routes/notes.test.ts
@@ -4,47 +4,41 @@ import { Note } from '../models/models';
 import initDB, { knex, resetDB } from '../models/initDB';
 import app from '../app';
 
+const base = '/api/notes';
+
 describe('Notes', () => {
-  beforeAll(async done => {
+  beforeAll(async (done) => {
     await resetDB();
     await initDB();
     done();
   });
 
-  afterAll(done => knex.destroy(done));
+  afterAll((done) => knex.destroy(done));
 
-  test('POST /', async () => {
+  test('POST /api/notes', async () => {
     const newNote: Partial<Note> = {
       content: 'Hello World!',
     };
 
-    const res = await request(app)
-      .post('/')
-      .send(newNote);
+    const res = await request(app).post(base).send(newNote);
 
     expect(res.status).toEqual(200);
     expect(res.body['id']).toBeGreaterThan(0);
   });
 
-  test('GET /', async () => {
-    const res = await request(app)
-      .get('/')
-      .send();
+  test('GET /api/notes/', async () => {
+    const res = await request(app).get(base).send();
 
     expect(res.status).toEqual(200);
     expect(res.body['notes'].length).toEqual(1);
   });
 
-  test('DELETE /:id', async () => {
-    let res = await request(app)
-      .delete(`/1`)
-      .send();
+  test('DELETE /api/notes/:id', async () => {
+    let res = await request(app).delete(`${base}/1`).send();
 
     expect(res.status).toEqual(200);
 
-    res = await request(app)
-      .get(`/`)
-      .send();
+    res = await request(app).get(base).send();
 
     expect(res.status).toEqual(200);
     expect(res.body['notes'].length).toEqual(0);
